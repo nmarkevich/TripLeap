9df6da7bace6371395d73c17478ed37635acdb13
nmarkevich <natallia.markevich@gmail.com>
2021-12-07 21:19:08 -0600

    feat: Add webpack, send user's input to the server

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+var path = require('path')
+const express = require('express')
+const cors = require ('cors')
+
+const app = express()
+app.use(cors())
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+// designates what port the app will listen to for incoming requests
+app.listen(8082, function () {
+    console.log('TripLeap app listening on port 8082!')
+})
+
+app.get('/tripInfo/:zc/:ld', infoForTrip);
+
+async function infoForTrip (req, res){
+  let zipCode = req.params.zc;
+  let leavingDate = req.params.ld;
+  console.log(zipCode, leavingDate);
+  res.send('ok');
+}
