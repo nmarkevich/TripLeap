a04eb8bb64d088a00d21f8df3e6d01accb04d2ce
Natallia Markevich <natallia.markevich@gmail.com>
2022-04-21 18:22:04 -0500

    refactor: Moving UpdateUI function to app.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
+import app from './js/app'
 export { handleSaveTrip }
 
 const addButton = document.getElementById("add");
@@ -35,7 +36,7 @@ function handleSaveTrip(event) {
     event.preventDefault();
     console.log("The trip duration: " + length);
     getInfoForTrip(location, leavingDate, length)
-      .then(results => updateUI(results));
+      .then(results => app.updateUI(results));
     outputForm.style.visibility="visible";
     document.getElementById("location").value = '';
     document.getElementById("leavingDate").value = '';
@@ -48,50 +49,4 @@ const getInfoForTrip = async(loc, ld, days) => {
   const res = await fetch(`http://localhost:8082/tripInfo/${loc}/${ld}/${days}`);
   const allTrips = res.json();
   return allTrips;
-}
-
-function updateUI (tripsInfo) {
-  try {
-    console.log(tripsInfo);
-    let trip = document.createElement("div");
-    let tripInfo = document.createElement("div");
-    trip.id = "result";
-    tripInfo.id = "info";
-    let pic = document.createElement("img");
-    let city = document.createElement("div");
-    let startDate = document.createElement("div");
-    let daysDiff = document.createElement("div");
-    let weather = document.createElement("div");
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    for (let i=0; i < tripsInfo.length; i++) {
-
-      const tripDate = new Date(tripsInfo[i].leavingDate);
-    // Calculate the time difference between the leaving date and today
-      const differenceTime = tripDate.getTime() - today.getTime();
-    // Calculate the number of days between two dates
-      const differenceDays = Math.round(differenceTime/(1000 * 3600 * 24));
-
-      pic.src = tripsInfo[i].pic;
-      city.innerHTML = "Trip to: " + tripsInfo[i].city + ', ' + tripsInfo[i].country;
-      trip.appendChild(pic);
-      tripInfo.appendChild(city);
-      startDate.innerHTML = "Departing: " + tripsInfo[i].leavingDate;
-      tripInfo.appendChild(startDate);
-      daysDiff.innerHTML = "Length of the trip: " + tripsInfo[i].daysDiff + " days";
-      tripInfo.appendChild(daysDiff);
-      if (differenceDays > 16) {
-        weather.innerHTML = "Typical Weather for then is: " + tripsInfo[i].temp + " F" + " (This is a forecast of the 16th day)";
-        tripInfo.appendChild(weather);
-      } else {
-        weather.innerHTML = "Typical Weather for then is: " + tripsInfo[i].temp + " F";
-        tripInfo.appendChild(weather);
-      }
-      trip.appendChild(tripInfo);
-      outputForm.appendChild(trip);
-    }
-  } catch (error) {
-    console.log("error", error);
-  }
 }
---
+++ src/client/js/app.js
@@ -0,0 +1,46 @@
+function updateUI (tripsInfo) {
+  try {
+    console.log(tripsInfo);
+    let trip = document.createElement("div");
+    let tripInfo = document.createElement("div");
+    trip.id = "result";
+    tripInfo.id = "info";
+    let pic = document.createElement("img");
+    let city = document.createElement("div");
+    let startDate = document.createElement("div");
+    let daysDiff = document.createElement("div");
+    let weather = document.createElement("div");
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    for (let i=0; i < tripsInfo.length; i++) {
+      const tripDate = new Date(tripsInfo[i].leavingDate);
+      // Calculate the time difference between the leaving date and today
+      const differenceTime = tripDate.getTime() - today.getTime();
+      // Calculate the number of days between two dates
+      const differenceDays = Math.round(differenceTime/(1000 * 3600 * 24));
+  
+      pic.src = tripsInfo[i].pic;
+      city.innerHTML = "Trip to: " + tripsInfo[i].city + ', ' + tripsInfo[i].country;
+      trip.appendChild(pic);
+      tripInfo.appendChild(city);
+      startDate.innerHTML = "Departing: " + tripsInfo[i].leavingDate;
+      tripInfo.appendChild(startDate);
+      daysDiff.innerHTML = "Length of the trip: " + tripsInfo[i].daysDiff + " days";
+      tripInfo.appendChild(daysDiff);
+      if (differenceDays > 16) {
+        weather.innerHTML = "Typical Weather for then is: " + tripsInfo[i].temp + " C" + " (This is a forecast of the 16th day)";
+        tripInfo.appendChild(weather);
+      } else {
+        weather.innerHTML = "Typical Weather for then is: " + tripsInfo[i].temp + " C";
+        tripInfo.appendChild(weather);
+      }
+      trip.appendChild(tripInfo);
+      outputForm.appendChild(trip);
+    }
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+export default { updateUI };
