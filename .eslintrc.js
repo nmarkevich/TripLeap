9967441be6ba7e940407f89a92a4416ad750e5df
Natallia Markevich <natallia.markevich@gmail.com>
2022-04-22 15:05:01 -0500

    chore: Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "standard"
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": "1",
+    "no-trailing-spaces": "2",
+    "quotes": ["error", "double"]
+  }
+}
