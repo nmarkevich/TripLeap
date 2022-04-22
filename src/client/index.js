b4afbb06967f9cdce9d0150b100eed437d68c12a
Natallia Markevich <natallia.markevich@gmail.com>
2022-04-22 13:43:29 -0500

    fix: Added file-loaded to fix images issue in prod

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
+import logo from './images/logo.jpg'
+import banner from './images/banner.jpg'
 export { handleSaveTrip } from './js/saveTripInfo'
 
 const addButton = document.getElementById("add");
--- webpack.prod.js
+++ webpack.prod.js
@@ -21,7 +21,18 @@ module.exports = {
         test: /\.(jpg|png)$/,
         use: {
           loader: "url-loader"
-        }
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
       }
     ]
   },
